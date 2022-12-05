c5c68a3653d3041ac3728051e379e15ef982ed8e
Paolo Martini <mrtnpaolo@protonmail.com>
2022-12-05 07:10:29 +0100

    🧼 day-06: clean up

--- execs/Day05.hs
+++ execs/Day05.hs
@@ -1,67 +1,38 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.List          qualified as L
-import Data.List.Split
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array         qualified as A
-import Debug.Trace
+import Advent                         ( getInput )
+import Data.Char                      ( isSpace, isDigit )
+import Data.List       qualified as L ( transpose, foldl', splitAt )
+import Data.List.Split                ( splitOn )
+import Data.Array      qualified as A ( listArray, (!), (//), elems )
 
 main =
   do inp <- getInput parse 5
-     putStrLn (part1 inp)
-     putStrLn (part2 inp)
+     putStrLn (uncurry part1 inp)
+     putStrLn (uncurry part2 inp)
+
+parse contents = (stacks,instrs)
   where
-    parse = p . splitOn "\n\n" . map clean
-    clean c
-      | c `elem` "[]" = ' '
-      | isLower c     = ' '
-      | otherwise     = c
-    p [s,instrs] = (stacks s,map toI (lines instrs))
+    [raw_stacks,raw_instrs] = splitOn "\n\n" contents
+    stacks =
+      (\xs -> A.listArray (1,length xs) xs) .
+      map (dropWhile isSpace . reverse . tail) . -- ["NZ","DCM,"P"]
+      filter (isDigit . head) . map reverse .    -- ["1NZ ","2DCM","3P  "]
+      L.transpose . lines $ raw_stacks
+    instrs =
+      map (map (read @Int) . words) .            -- [[1,2,1],...]
+      map (map (\case c | isDigit c -> c | otherwise -> ' ')) .
+      lines $ raw_instrs
 
-stacks xs = a
+part1 = solve reverse
+
+part2 = solve id
+
+solve f stacks instrs =
+  map head . A.elems $ L.foldl' (go f) stacks instrs
+
+go f stacks [n,from,to] = stacks A.// [(from,xs'),(to,f zs<>ys)]
   where
-    ys = map (filter (not . isSpace) . concat) . L.transpose $ map simpl $ init $ lines xs
-    simpl = chunksOf 4
-    a :: A.Array Int [Char]
-    a = A.listArray (1,len) ys
-    len = length ys
-
-toI xs = let [n,from,to] = map (read @Int) (words xs) in (n,from,to)
-
-go ss [] = ss
-go ss ((n,from,to):rs) = go ss' rs
-  where
-    ss' = go' n from to ss
-
-go' 0 _ _ ss = ss
-go' n f t ss = go' (n-1) f t ss2
-  where
-    (x:xs) = ss A.! f
-    ss1 = ss A.// [(f,xs)]
-    ys = ss A.! t
-    ss2 = ss1 A.// [(t,x:ys)]
-
-part1 (ss,instrs) = map head $ A.elems $ go ss instrs
-
-part2 (ss,instrs) = map head $ A.elems $ go2 ss instrs
-
-go2 ss [] = ss
-go2 ss ((n,from,to):rs) = go2 ss' rs
-  where
-    ss' = go2' n from to ss
-
-go2' n f t ss = ss2
-  where
-    (ls,rs) = splitAt n (ss A.! f)
-    ss1 = ss A.// [(f,rs)]
-    ys = ss A.! t
-    ss2 = ss1 A.// [(t,ls++ys)]
+    xs = stacks A.! from
+    ys = stacks A.! to
+    (zs,xs') = L.splitAt n xs
