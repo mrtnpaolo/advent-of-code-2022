c17c6e46f431fae2f0e4c22b78084bc684c8a1c1
Paolo Martini <mrtnpaolo@protonmail.com>
2022-12-08 09:24:39 +0100

    👨‍🔧 day-08: clean up a lil more

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -1,34 +1,30 @@
 module Main ( main ) where
 
-import Advent.List  ( count )
-import Advent.Input ( getInputArray )
-import Advent.Coord ( above, right, below, left )
+import Advent ( count, getInputArray, above, right, below, left )
 
-import Data.Ix                           ( range, inRange )
-import Data.Array.Unboxed qualified as A ( amap, (!), bounds )
+import Data.Array.Unboxed ( Ix( range, inRange ), amap, (!), bounds )
 
 main =
-  do inp <- A.amap (read . pure) <$> getInputArray 8
+  do inp <- getInputArray 8
      print (part1 inp)
      print (part2 inp)
 
-part1 a = count (visible a) (range (A.bounds a))
+part1 a = count (visible a) (range (bounds a))
 
-visible a c@((a A.!) -> n) = any (all (< n)) (trees a c)
+visible a c@((a !) -> n) = any (all (< n)) (trees a c)
 
-trees a c = [ [ a A.! c' | c' <- cs ] | cs <- rays a c ]
+trees a c = [ [ a ! c' | c' <- cs ] | cs <- rays a c ]
 
 rays a c = takeWhile (inside a) <$>
   [ tail (iterate dir c) | dir <- [above,right,below,left] ]
 
-inside a = inRange (A.bounds a)
+inside a = inRange (bounds a)
 
-part2 a = maximum [ product (views a c) | c <- range (A.bounds a) ]
+part2 a = maximum [ product (views a c) | c <- range (bounds a) ]
 
-views a c@((a A.!) -> n) = [ view n ts | ts <- trees a c ]
+views a c@((a !) -> n) = [ view n ts | ts <- trees a c ]
 
-view :: Int -> [Int] -> Int
-view _ []     = 0
+view _ []     = 0 :: Int
 view n (x:xs)
   | x >= n    = 1
   | x <  n    = 1 + view n xs
