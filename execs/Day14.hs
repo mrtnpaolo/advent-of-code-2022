e5d3fe261142ba92c65ceb00e9728afd3f8000bb
Paolo Martini <mrtnpaolo@protonmail.com>
2022-12-14 09:10:53 +0100

    🧼 day-14: clean up

--- execs/Day14.hs
+++ execs/Day14.hs
@@ -1,54 +1,50 @@
-module Main (main) where
+module Main ( main ) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent     ( getInputLines, count, Coord(..), below, left, right )
+import Data.Ix    ( range )
+import Data.Maybe ( listToMaybe )
+import Data.List  ( unfoldr, foldl' )
+import Data.Map.Strict qualified as M
+  ( unions, keys, elems, empty, union, fromList, notMember, insert )
 
 main =
-  do inp <- M.unions . (M.singleton begin '+' :) <$> getInputLines parse 14
-     putStrLn (drawCoords inp)
-     print (part2 inp)
+  do rocks <- M.unions <$> getInputLines parse 14
+     let floor = 2 + maximum [ y | C y _ <- M.keys rocks ]
+     print (part1 floor rocks)
+     print (part2 floor rocks)
   where
     parse = path . words . map \case c | c `elem` ",->" -> ' '; c -> c
-    path = L.foldl' go M.empty . (\xs -> zip xs (tail xs)) . split
+    path = foldl' line M.empty . (\xs -> zip xs (tail xs)) . split
       where
-        split [] = []
-        split (x:y:xs) = C (read y) (read x) : split xs
-        go m (a,b) = M.union m $ M.fromList [ (c,'#') | c <- range bounds ]
+        split (x:y:xs) = C (read y) (read x) : if null xs then [] else split xs
+        line m (a,b) = M.union m $ M.fromList [ (c,'#') | c <- range bounds ]
           where
             bounds = (min a b,max a b)
 
 begin = C 0 500
 
-pour m = begin : path begin
+pour m from = unfoldr next from
   where
-    floor = 2 + maximum [ y | (C y _,'#') <- M.toList m ]
+    next c = listToMaybe [ (d,d) | d <- dests c, d `M.notMember` m ]
+    dests c = [below c,left (below c),right (below c)]
 
-    path c@(C y x)
-      | Just d <- listToMaybe [ d | d <- dests c, free d ] = d : path d
-      | otherwise = []
-
-    free c@(C y _) = y < floor && M.notMember c m
-
-dests c = [below c,left (below c),right (below c)]
-
-
-part2 = go
+part1 floor m = go m
   where
     go m
-      | d /= begin = go (M.insert d 'o' m)
-      | otherwise = count ('o'==) [ x | (c,x) <- M.toList m ]
+      | any (>= floor) [ y | C y _ <- path ] = count ('o'==) (M.elems m)
+      | otherwise = go (M.insert dest 'o' m)
       where
-        path = pour m
-        d = last path
+        path = pour m begin
+        dest = last path
+
+part2 floor m = go m (pourOnTheFloor m begin)
+  where
+    go m path@(dest:(~(next:rest)))
+      | path == [begin] = count ('o'==) (M.elems $ M.insert begin 'o' m)
+      | otherwise = go m' (pourOnTheFloor m' next ++ rest)
+      where
+        m' = M.insert dest 'o' m
+
+    pourOnTheFloor m from = reverse path
+      where
+        path = from : takeWhile (\(C y _) -> y < floor) (pour m from)
